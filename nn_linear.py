71c1f3848036b2af0de9b3ca75cd92f657106b17
Zhou Jiale <zjl256904767@outlook.com>
2023-12-08 19:27:05 +0800

    nn_linear

--- nn_linear.py
+++ nn_linear.py
@@ -1,4 +1,33 @@
 import torchvision
 import torch
+from torch import nn
+from torch.nn import Linear
 dataset = torchvision.datasets.CIFAR10(root="./data", train=False, download=True, transform=torchvision.transforms.ToTensor())
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=64, shuffle=True)
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=64, shuffle=True,drop_last=True)
+
+#img,target= dataloader[0]
+#print(img.shape)
+
+class Demo(nn.Module):
+    def __init__(self):
+        super(Demo,self).__init__()
+        self.Linear1=Linear(64*3*32*32,10)
+
+    def forward(self,input):
+        output = self.Linear1(input)
+        return output
+
+
+demo = Demo()
+
+
+
+for data in dataloader:
+    imgs,targets = data
+    print(imgs.shape)
+    input = torch.flatten(imgs) # 将图片压缩成一维
+    print(input.shape)
+    output = demo(input)
+    print(output.shape)
+
+
